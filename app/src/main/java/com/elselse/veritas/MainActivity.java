667c49919eaaed3a6ce4fab6fe7daa0862b19e82
actuallyroy <amitkumar93525@gmail.com>
2022-12-12 00:22:46 +0530

    initial commit

--- app/src/main/java/com/elselse/veritas/MainActivity.java
+++ app/src/main/java/com/elselse/veritas/MainActivity.java
@@ -1,22 +1,24 @@
 package com.elselse.veritas;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageProxy;
-
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.divider.MaterialDividerItemDecoration;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
@@ -28,17 +30,39 @@ import java.io.IOException;
 import java.util.Date;
 
 public class MainActivity extends Activity {
-
+    boolean b = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView mask = findViewById(R.id.mask);
+        MaterialButton searchBtn = findViewById(R.id.searchBtn),
+                castBtn = findViewById(R.id.castBtn),
+                imageBtn = findViewById(R.id.imageBtn),
+                notice_ok = findViewById(R.id.notice_ok),
+                queryBtn = findViewById(R.id.queryBtn),
+                clipboardBtn = findViewById(R.id.clipboardBtn);
+        TextInputLayout searchField = findViewById(R.id.searchField);
+        FrameLayout notice = findViewById(R.id.notice);
 
-        MaterialButton searchBtn = findViewById(R.id.searchBtn), castBtn = findViewById(R.id.castBtn), imageBtn = findViewById(R.id.imageBtn);
+        SharedPreferences prefs = getSharedPreferences("com.elselse.veritas", MODE_PRIVATE);
+        if (prefs.getBoolean("firstRun", true)) {
+            notice.setVisibility(View.VISIBLE);
+            prefs.edit().putBoolean("firstRun", false).apply();
+        }
+
+        notice_ok.setOnClickListener(v -> notice.setVisibility(View.GONE));
+
 
 
 
         searchBtn.setOnClickListener(v -> {
+            b = true;
+            searchField.animate().y(10).setDuration(500).start();
+            searchField.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+            new Handler().postDelayed(() -> mask.setVisibility(View.VISIBLE), 300);
 
         });
 
@@ -53,6 +77,40 @@ public class MainActivity extends Activity {
             intent.setType("image/*");
             startActivityForResult(intent, 1);
         });
+
+        queryBtn.setOnClickListener(v -> {
+            b = true;
+            searchField.animate().y(10).setDuration(500).start();
+            searchField.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+            new Handler().postDelayed(() -> mask.setVisibility(View.VISIBLE), 300);
+        });
+
+        clipboardBtn.setOnClickListener(v -> {
+            ClipboardManager myCP;
+            myCP = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+            ClipData mydata= myCP.getPrimaryClip();
+            ClipData.Item item= mydata.getItemAt(0);
+            String mytext = String.valueOf(item.getText());
+            Log.d("MainActivity", "Clipboard text: " + mytext);
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(b){
+            b = false;
+            TextInputLayout searchField = findViewById(R.id.searchField);
+            searchField.animate().y(10000).setDuration(500).start();
+            searchField.clearFocus();
+            ImageView mask = findViewById(R.id.mask);
+            new Handler().postDelayed(() -> mask.setVisibility(View.GONE), 300);
+
+        }else{
+            super.onBackPressed();
+        }
     }
 
     @Override
